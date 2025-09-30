854abc53ffc73fabdbe931b6312abebb70b813b9
DS4506 <davidsmith4506@gmail.com>
2025-09-29 20:20:06 -0500

    Initial Commit

---
+++ OrientationVisualizer/OrientationVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationVisualizerApp.swift
+//  OrientationVisualizer
+//
+//  Created by Willie Earl on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct OrientationVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
